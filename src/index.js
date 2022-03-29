b605f423e638449c6df1e1f7c81218c596e3ad56
Lando Vargas <lando_vargas@outlook.com>
2022-03-29 13:49:30 -0300

    se agrega redirect al /message desde root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const messageRouter = require("./routers/messages");
 
+app.use(cors({origin: "*"}));
 app.use(express.json())
 app.use(messageRouter)
+
+app.get("/", (req, res) => res.redirect("/message"));
 
 const port = process.env.PORT || 8080;
 
